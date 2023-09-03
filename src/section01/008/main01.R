db852c9c11e8d3204cc4b688dbfe9c6b21a0040b
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-03 12:03:06 +0900

    refactor section01

--- src/section01/008/main01.R
+++ src/section01/008/main01.R
@@ -1,6 +1,7 @@
 readline() |>
   str_split_1("") |>
   as_tibble() |>
+  # The arguement of utf8ToInt takes only scalar value, so we need to do process rowwise
   rowwise() |>
   mutate(code = utf8ToInt(value), code2 = if_else(str_detect(value, "[a-z]"), 219-code, code), ans = intToUtf8(code2)) |>
   pull(ans) |>
--- src/section01/009/main01.R
+++ src/section01/009/main01.R
@@ -1,11 +1,13 @@
-tb <- str |>
+"I couldn’t believe that I could actually understand what I was reading : the phenomenal power of the human mind ." |>
   word(1:100) |>
   as_tibble() |>
-  drop_na()
-new <- tb |>
-  rowwise() |>
+  drop_na() |>
   mutate(p1 = str_sub(value, 1,1), p2 = str_sub(value, 2,-2), p3 = if_else(str_length(value)>1, str_sub(value, -1), "")) |>
-  mutate(new = str_c(p1, sample(p2 |> str_split_1("")) |> str_flatten(""), p3) ) |>
+  # Without rowwise, str_split returns list, make it complicated.
+  rowwise() |>
+  mutate(p2_shuf = sample(p2 |> str_split_1("")) |> str_flatten("")) |>
+  ungroup() |>
+  mutate(new = str_c(p1, p2_shuf, p3) ) |>
   pull(new) |>
   str_flatten(" ") |>
   print()
