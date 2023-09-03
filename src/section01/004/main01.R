d7354958bae2354859667cb494fec1d3092199aa
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-03 11:09:15 +0900

    fix 004

--- src/section01/004/main01.R
+++ src/section01/004/main01.R
@@ -1,8 +1,8 @@
 "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can." |>
   word(1:100) |>
   as_tibble() |>
-  mutate(pick2 = row_number() %in% c(1, 5, 6, 7, 8, 9, 15, 16, 19), pos = row_number()) |>
   drop_na() |> 
+  mutate(pos = row_number(), pick2 = pos %in% c(1, 5, 6, 7, 8, 9, 15, 16, 19)) |>
   mutate(sub = str_sub(value, 1, 2-pick2)) |>
   select(sub, pos) |>
-  print()
+  print(n=Inf)
