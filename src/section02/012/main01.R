5d15974a11bfb09e095aabef3927c2ab9a4ba652
Kenta_Nakajima <knknkn1162@gmail.com>
2023-09-18 09:58:17 +0900

    refactor

--- src/section02/012/main01.R
+++ src/section02/012/main01.R
@@ -1,6 +1,6 @@
 tb <- read_tsv("https://nlp100.github.io/data/popular-names.txt", col_names=c("name", "sex", "numberOfPeople", "year"))
 1:2 |>
-  map(\(x)
-    select(tb, x) |>
-      write_tsv(str_glue("col{x}.txt"))
+  map(\(x) tb |>
+    select(x) |>
+    write_tsv(str_glue("col{x}.txt"))
   )
